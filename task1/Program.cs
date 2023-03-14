ce7890ee9cc0456de26dbee8f7b43574bbcbb892
Vitaliy Gunbin <vitalik_gunbin@mail.ru>
2023-03-14 13:54:16 +0300

    task completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,11 +1,31 @@
 ﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется
 //пользоваться коллекциями, лучше обойтись исключительно массивами. 
+Console.Clear();
+System.Console.WriteLine("Введите количество элементов массива: ");
+int number = int.Parse(Console.ReadLine());
+string[] massive = new string[number];
+for (int i = 0; i < number; i++)
+{
+    System.Console.WriteLine("Введите значение: ");
+    massive[i] = Console.ReadLine();
+}
+System.Console.Write("Введеный вами массив получился таким: [");
+System.Console.Write(String.Join(", ", massive));
+System.Console.WriteLine("]");
+System.Console.WriteLine();
+System.Console.Write("Значения, длина которых меньше 3 символов = [");
+for (int i = 0; i < number; i++)
+{
+    if (massive[i].Length <= 3)
+    {
+        System.Console.Write($"{massive[i]} ");
+    }
+}
+System.Console.WriteLine("]");
 
-System.Console.WriteLine("Enter");
-string firstValue = Console.ReadLine();
-string secondValue = Console.ReadLine();
-string thirdValue = Console.ReadLine();
+
+
 
 
 
